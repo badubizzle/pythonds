415a298ec20651444395049a94423b24f16daab7
Oparah Dimkpa <Oparahdc@gmail.com>
2019-07-17 14:26:36 +0100

    added remove head and added add_head functionality to stack.py

--- lesson2/python_linked_list.py
+++ lesson2/python_linked_list.py
@@ -1,29 +1,29 @@
 class Node(object):
 
     def __init__(self, value, next=None):
-        self.value=value
+        self.value = value
         if isinstance(next, Node):
-            self.next=next
+            self.next = next
         else:
-            self.next = None        
-    
+            self.next = None
+
 
 class LinkedList(object):
 
     def __init__(self):
         self.root = None
-    
+
     def add(self, node):
         if self.root is None:
             self.root = node
             return
-        
+
         current = self.root
         while current.next is not None:
             current = current.next
-        
+
         current.next = node
-    
+
     def print_all(self):
         current = self.root
         while current is not None:
@@ -31,14 +31,9 @@ class LinkedList(object):
             current = current.next
 
     def insert_at(self, value, index):
-        
-        
-        node = Node(value)        
-        current = self.root
 
         # 1->2->3
         # insert 4  at index 2
-
 
         # index = 2
         # count = 1
@@ -54,42 +49,41 @@ class LinkedList(object):
         # 4->3
         # 2->4->3
 
-
-
+        node = Node(value)
+        current = self.root
 
         if index == 0:
             if self.root is None:
                 self.root = node
                 return
             node.next = self.root
+            self.root = node
             return
 
         count = 1
         while current is not None and current.next is not None:
-            if index == count:                
+            if index == count:
                 node.next = current.next
                 current.next = node
                 return
 
-            current = current.next 
+            current = current.next
             count = count + 1
-        
+
         current.next = node
-        
-        
 
     def find_at_index(self, index):
 
         current = self.root
-        count = 0 # counter to point to index
+        count = 0  # counter to point to index
         while current is not None:
             if index == count:
                 return current.value
             count = count + 1
             current = current.next
-        
+
         return None
-        
+
 
 l = LinkedList()
 for v in range(0, 10):
@@ -99,4 +93,3 @@ for v in range(0, 10):
 
 print(l.find_at_index(3))
 l.print_all()
-
--- lesson3/stack.py
+++ lesson3/stack.py
@@ -3,16 +3,16 @@ class MyStack(object):
 
     def __init__(self):
         self.data = []
-    
+
     def pop(self):
         self.data.pop()
-    
+
     def push(self, value):
         self.data.append(value)
-    
+
     def empty(self):
         len(self.data) == 0
-    
+
     def size(self):
         len(self.data)
 
@@ -20,70 +20,91 @@ class MyStack(object):
 class Node(object):
 
     def __init__(self, value, next=None):
-        self.value=value
+        self.value = value
         if isinstance(next, Node):
-            self.next=next
+            self.next = next
         else:
-            self.next = None        
-    
+            self.next = None
+
 
 class LinkedList(object):
 
     def __init__(self, head=None):
+        self.size = 0
         if isinstance(head, Node):
             self.head = head
+            self.size = 1
         else:
             self.head = None
-    
-    def add(self, value):        
-        if self.head is None:            
+
+    def add(self, value):
+        if self.head is None:
             new_node = Node(value)
             self.head = new_node
-            return         
-        
-        pointer = self.head        
-        while pointer.next is not None:        
+            return
+
+        pointer = self.head
+        while pointer.next is not None:
             pointer = pointer.next
-        
+
         new_node = Node(value)
         pointer.next = new_node
-    
+
     def print_all(self):
         current = self.head
         while current is not None:
             print(current.value)
             current = current.next
-    
-    
-    
+
+    def add_head(self, value):
+        node = Node(value)
+
+        node.next = self.head
+        self.head = node
+        self.size += 1
+
+    def remove_head(self):
+        current = self.head
+
+        if current is None:
+            return 'Empty list'
+
+        self.head = current.next
+        self.size -= 1
+        return current.value
+
+
 class LinkedStack():
 
     def __init__(self):
         self.linked_list = LinkedList()
-    
+
     def pop(self):
         """
         Get the top element in the stack
         """
-                
+        return self.linked_list.remove_head()
 
     def push(self, value):
         """
         Add to top of stack
         """
-        self.linked_list.add(value)        
-    
+        self.linked_list.add_head(value)
+
     def empty(self):
         pass
-    
+
     def print_all(self):
         self.linked_list.print_all()
 
 
 stack = LinkedStack()
 
-for v in range(0, 20):
+for v in range(0, 5):
     stack.push(v+1)
 
+# stack.print_all()
+stack.pop()
+stack.pop()
+stack.pop()
 stack.print_all()
-
